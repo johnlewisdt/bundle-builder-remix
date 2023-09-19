ef047803e6bf94768e0d42850c0c6c7d87ee45d8
johnlewisdt <john.lewis@statementagency.com>
2023-09-19 13:18:36 +0100

    Build bundle interface
    Todo: return list of all bundle products instead of object

--- app/routes/app.buildbundle.jsx
+++ app/routes/app.buildbundle.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { json } from "@remix-run/node";
 import {
   useActionData,
@@ -9,16 +9,22 @@ import {
 import {
   Page,
   Layout,
+  FormLayout,
   Text,
+  TextField,
+  InlineError,
   VerticalStack,
   Card,
+  Bleed,
   Button,
   HorizontalStack,
   Box,
   Divider,
   List,
   Link,
+  Thumbnail,
 } from "@shopify/polaris";
+import { ImageMajor } from "@shopify/polaris-icons";
 
 import { authenticate } from "../shopify.server";
 
@@ -28,44 +34,60 @@ export const loader = async ({ request }) => {
   return json({ shop: session.shop.replace(".myshopify.com", "") });
 };
 
+// async function selectProduct() {
+//   const products = await window.shopify.resourcePicker({
+//     type: "product",
+//     action: "select", // customized action verb, either 'select' or 'add',
+//   });
+//   if (products) {
+//     const { images, id, variants, title, handle } = products[0];
+
+//     const resultage = {
+//       productId: id,
+//       productVariantId: variants[0].id,
+//       productTitle: title,
+//       productHandle: handle,
+//       productAlt: images[0]?.altText,
+//       productImage: images[0]?.originalSrc,
+//     };
+
+//     return resultage;
+//   }
+// }
+
 export async function action({ request }) {
   const { admin } = await authenticate.admin(request);
 
-  const color = ["Red", "Orange", "Yellow", "Green"][
-    Math.floor(Math.random() * 4)
-  ];
+  
   const response = await admin.graphql(
     `#graphql
-      mutation populateProduct($input: ProductInput!) {
+      mutation CreateProductBundle($input: ProductInput!) {
         productCreate(input: $input) {
-          product {
-            id
+          product{
             title
-            handle
-            status
             variants(first: 10) {
-              edges {
-                node {
+              edges{
+                node{
                   id
-                  price
-                  barcode
-                  createdAt
                 }
               }
             }
           }
+          userErrors{
+            field
+            message
+          }
         }
       }`,
-    {
-      variables: {
-        input: {
-          title: `${color} Snowboard`,
-          variants: [{ price: Math.random() * 100 }],
-        },
-      },
-    }
+      {
+        variables: {
+          input: {
+            title: "The reeeee graphQL Bundle",
+          }
+        }
+      }
   );
-
+  
   const responseJson = await response.json();
 
   return json({
@@ -73,36 +95,106 @@ export async function action({ request }) {
   });
 }
 
-export default function Index() {
+export default function BuildBundle() {
+  
+  const [bundleTitle, setBundleTitle] = useState("");
+  const [bundlePrice, setBundlePrice] = useState("");
+
   const nav = useNavigation();
   const { shop } = useLoaderData();
   const actionData = useActionData();
+
+  const productForm =  useLoaderData();
+  const errors = useActionData()?.errors || {};
+
+  const [formState, setFormState] = useState(productForm);
+  const [cleanFormState, setCleanFormState] = useState(productForm);
+  const isDirty = JSON.stringify(formState) !== JSON.stringify(cleanFormState);
+
+  const handleBundleTitleChange = (/** @type {React.SetStateAction<string>} */ value) => setBundleTitle(value);
+  const handleBundlePriceChange = (/** @type {React.SetStateAction<string>} */ value) => setBundlePrice(value);
+  // const handleSubmit = (event) => console.log(event) 
+
   const submit = useSubmit();
 
   const isLoading =
     ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
 
-  const productId = actionData?.product?.id.replace(
+  const productId = actionData?.product?.id?.replace(
     "gid://shopify/Product/",
     ""
   );
 
+  async function selectProduct() {
+  
+    const products = await window.shopify.resourcePicker({
+      type: "product",
+      multiple: true,
+      action: "select", // customized action verb, either 'select' or 'add',
+    });
+  
+    if (products) {
+      const { images, id, variants, title, handle } = products[0];
+  
+        setFormState({
+          ...formState,
+        productId: id,
+        productVariantId: variants[0].id,
+        productTitle: title,
+        productHandle: handle,
+        productAlt: images[0]?.altText,
+        productImage: images[0]?.originalSrc,
+      });
+      
+      const BundleProducts = () => {
+        const bundledProducts = Object.values(formState);
+        return (
+          bundledProducts.map((bundleItem,index) => 
+            <HorizontalStack key={index} blockAlign="center" gap={"5"}>
+              <Thumbnail
+                source={bundleItem.productImage || ImageMajor}
+                alt={bundleItem.productAlt}
+              />
+              <Text as="span" variant="headingMd" fontWeight="semibold">
+                {bundleItem.productTitle}
+              </Text>
+            </HorizontalStack>
+          )
+        )
+      }
+
+    }
+    console.log(products);
+    
+  }
+ 
+
   useEffect(() => {
     if (productId) {
-      shopify.toast.show("Product created");
+      shopify.toast.show("Bundle created");
+      setBundleTitle("");
+      setBundlePrice("");
     }
   }, [productId]);
-
-  const generateProduct = () => submit({}, { replace: true, method: "POST" });
-
+  
+  const CreateProductBundle = () => {
+    const data = {
+      title: formState.title,
+      productId: formState.productId || "",
+      productVariantId: formState.productVariantId || "",
+      productHandle: formState.productHandle || "",
+      destination: formState.destination,
+    };
+    submit({bundleTitle, bundlePrice, data}, { replace: false, method: "POST" });
+  } 
 
   return (
     <Page>
       <ui-title-bar title="Build a bundle">
-        <button variant="primary" onClick={generateProduct}>
+        <button variant="primary" onClick={CreateProductBundle}>
           Generate a product
         </button>
-        <button onClick={generateProduct}>
+        <button onClick={CreateProductBundle}>
           Build a bundle
         </button>
       </ui-title-bar>
@@ -113,45 +205,70 @@ export default function Index() {
               <VerticalStack gap="5">
                 <VerticalStack gap="2">
                   <Text as="h2" variant="headingMd">
-                    Current bundles
+                    Parent bundle item
                   </Text>
-                  <Text variant="bodyMd" as="p">
-                    This embedded app template uses{" "}
-                    <Link
-                      url="https://shopify.dev/docs/apps/tools/app-bridge"
-                      target="_blank"
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    Why not{" "}
-                    <Link url="/app/buildbundle">
-                      Build a bundle
-                    </Link>
-                    ? More info here: {" "}
-                    <Link
-                      url="https://shopify.dev/docs/api/admin-graphql"
-                      target="_blank"
-                    >
-                      Admin GraphQL
-                    </Link>{" "}
-                    .
-                  </Text>
+                  <FormLayout>
+                  <TextField
+                    label="Bundle Title"
+                    type="text"
+                    autoComplete="off"
+                    onChange={handleBundleTitleChange}
+                    value={bundleTitle}
+                  />
+                  <TextField
+                    label="Price"
+                    type="number"
+                    autoComplete="off"
+                    onChange={handleBundlePriceChange}
+                    value={bundlePrice}
+                  />
+                  </FormLayout>
                 </VerticalStack>
                 <VerticalStack gap="2">
                   <Text as="h3" variant="headingMd">
-                    Get started with products
+                    Add some products
                   </Text>
                   <Text as="p" variant="bodyMd">
-                    Generate a product with GraphQL and get the JSON output for
-                    that product. Learn more about the{" "}
-                    <Link
-                      url="https://shopify.dev/docs/api/admin-graphql/latest/mutations/productCreate"
-                      target="_blank"
-                    >
-                      productCreate
-                    </Link>{" "}
-                    mutation in our API references.
+                    Add products to your bundle using the product picker.
                   </Text>
+                  <Card>
+              <VerticalStack gap="2">
+                <HorizontalStack align="space-between">
+                  {formState.productId ? (
+                    <Button plain onClick={selectProduct}>
+                      Change product
+                    </Button>
+                  ) : null}
+                </HorizontalStack>
+                {formState.productId ? (
+                  {BundleProducts}
+                ) : (
+                  <VerticalStack gap="2">
+                    <Button onClick={selectProduct} id="select-product">
+                      Select product
+                    </Button>
+                    {errors.productId ? (
+                      <InlineError
+                        message={errors.productId}
+                        fieldID="myFieldID"
+                      />
+                    ) : null}
+                  </VerticalStack>
+                )}
+                <Bleed marginInline="20">
+                  <Divider />
+                </Bleed>
+                <HorizontalStack
+                  gap="5"
+                  align="space-between"
+                  blockAlign="start"
+                >
+                  
+                </HorizontalStack>
+              </VerticalStack>
+            </Card>
+
+
                 </VerticalStack>
                 <HorizontalStack gap="3" align="end">
                   {actionData?.product && (
@@ -162,8 +279,8 @@ export default function Index() {
                       View product
                     </Button>
                   )}
-                  <Button loading={isLoading} primary onClick={generateProduct}>
-                    Generate a product
+                  <Button loading={isLoading} primary onClick={CreateProductBundle}>
+                    Generate a bundle
                   </Button>
                 </HorizontalStack>
                 {actionData?.product && (
@@ -183,6 +300,7 @@ export default function Index() {
               </VerticalStack>
             </Card>
           </Layout.Section>
+          
           <Layout.Section secondary>
             <VerticalStack gap="5">
               <Card>
